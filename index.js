94073bf78187d300001fd3b8e67a2cb0f8236308
tecklaw99 <teck0899@gmail.com>
2025-07-14 17:17:30 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 import express from 'express';
 import fetch from 'node-fetch';
 

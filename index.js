fd8388b17b428771736311fffabda01ae41027ea
tecklaw99 <teck0899@gmail.com>
2025-08-07 13:58:30 +0000

    Your commit message here

--- index.js
+++ index.js
@@ -265,7 +265,7 @@ app.get('/check', (req, res) => {
 
   if (!lastSeen.has(sessionId)) {
     // no heartbeat seen yet (or we cleared it after the last trigger)
-    return res.json({ trigger: false });
+    return res.json({ sessionId });
   }
 
   const last = lastSeen.get(sessionId);
